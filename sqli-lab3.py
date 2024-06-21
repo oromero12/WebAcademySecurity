a07e6e34ba4f406c2b5642f0d49e47b9464e5f36
oromero12 <oscar.romero.villa@gmail.com>
2024-06-21 21:12:50 +0000

    sqli_lab3

--- sqli-lab3.py
+++ sqli-lab3.py
@@ -1,6 +1,6 @@
 import requests
 import sys
-import urllib3
+import urllib3 #
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
